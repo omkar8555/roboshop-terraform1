e9f0bf4429ebed0141f06cb2ead6d1cd260194cc
lenovo <omkar.reddy211@gmailcom>
2024-11-08 10:34:28 +0530

    payment script

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -46,7 +46,7 @@ resource "aws_instance" "instance" {
 
       inline = [
         "sudo labauto ansible",
-        "ansible-pull -i localhost, -U https://github.com/omkar8555/learn-ansible.1.git roboshop.yml -e env=$(var.env) -e app_name=$(component_name)"
+        "ansible-pull -i localhost, -U https://github.com/omkar8555/learn-ansible.1.git roboshop.yml -e env=${var.env} -e app_name=${component_name}"
       ]
     }
 }
