d419b19c8941265c8958b6c69b0b2675adad298d
lenovo <omkar.reddy211@gmailcom>
2024-11-08 10:39:01 +0530

    payment script

--- main.tf
+++ main.tf
@@ -1,24 +1,23 @@
 module "app_instances" {
-    for_each = var.app_instances
-    depends_on = [module.db_instances]
-    source = "./modules/ec2"
-    env    =  var.env
-    component_name = each.key
-    instance_type  = each.value["instance_type"]
-    app_port       = each.value["app_port"]
-    zone_id        = var.zone_id
-    domain_name    = var.domain_name
-
-    }
+  depends_on     = [module.db_instances]
+  for_each       = var.app_instances
+  source         = "./modules/ec2"
+  env            = var.env
+  app_port       = each.value["app_port"]
+  component_name = each.key
+  instance_type  = each.value["instance_type"]
+   domain_name   = var.domain_name
+    zone_id      = var.zone_id
+}
 
 module "web_instances" {
-    for_each = var.web_instances
-    depends_on = [module.app_instances]
-    source = "./modules/ec2"
-    env    =  var.env
-    component_name = each.key
-    instance_type  = each.value["instance_type"]
-    app_port       = each.value["app_port"]
-    zone_id        = var.zone_id
-    domain_name    = var.domain_name
-    }
+  depends_on     = [module.app_instances]
+  for_each       = var.web_instances
+  source         = "./modules/ec2"
+  env            = var.env
+  app_port       = each.value["app_port"]
+  component_name = each.key
+  instance_type  = each.value["instance_type"]
+   domain_name   = var.domain_name
+    zone_id      = var.zone_id
+}
